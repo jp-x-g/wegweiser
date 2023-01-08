295d77c36b5ca1605bf12b938b4a4ef7184fdae6
jp-x-g <jpxg-dev@protonmail.com>
2023-01-07 18:15:37 -0800

    start working on uploader

---
+++ uploader.py
@@ -0,0 +1,14 @@
+# JPxG 2023 01 07
+import datetime
+import requests
+import json
+import urllib
+import luadata
+import sys
+# python -m pip install --upgrade luadata
+# These ones are from the same directory.
+import lua_wrangler
+import article_fetcher
+
+headers = {"User-Agent": "JPxG's hoopty script (https://en.wikipedia.org/wiki/User:JPxG)"}
+
